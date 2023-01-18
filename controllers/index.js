e1da03e73bccd64b0acbb6681c5fd51b2d32475b
Chris Campbell <christopherecampbell@gmail.com>
2023-01-18 15:59:26 -0700

    update root index for logged in users to show their owned recipes and cookbooks

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,17 @@
 import { Cookbook } from "../models/cookbook.js"
+import { Recipe } from "../models/recipe.js"
 
 function index(req, res) {
   if(req.user){
     Cookbook.find({owner: req.user.profile._id})
     .then (cookbooks => {
-      res.render('index', {
-        title: "Butter",
-        cookbooks
+      Recipe.find({author: req.user.profile._id})
+      .then(recipes => {
+        res.render('index', {
+          title: "Butter",
+          cookbooks,
+          recipes
+        })
       })
     })
     .catch(err => {
