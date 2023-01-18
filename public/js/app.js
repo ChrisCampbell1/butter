c92ba44032c0575b67140e32c1e30549ba99a6b1
Chris Campbell <christopherecampbell@gmail.com>
2023-01-18 14:42:09 -0700

    add mobile menu

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,18 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const mobileMenuBtn = document.querySelector('#mobile-menu-btn')
+const mobileMenuEl = document.querySelector('#mobile-menu')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+mobileMenuBtn.addEventListener('click', showMenu)
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function showMenu(evt) {
+  if(mobileMenuEl.classList.contains("visible")){
+    mobileMenuEl.classList.remove("visible")
+  } else mobileMenuEl.classList.add("visible")
+}
